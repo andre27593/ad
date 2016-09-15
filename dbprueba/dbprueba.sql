edced76b594b6a49708a2ab99fc53dc2133dac37
andre27593 <dam.aolivero@institutoserpis.org>
2016-09-15 09:31:22 +0200

    Añadido dbprueba.sql

--- dbprueba/dbprueba.sql
+++ dbprueba/dbprueba.sql
@@ -3,3 +3,7 @@ create table categoria (
 	nombre varchar(50) unique
 
 );
+
+insert into categoria (nombre) values('categoria 1');
+insert into categoria (nombre) values('categoria 2');
+insert into categoria (nombre) values('categoria 3');
