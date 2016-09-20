36820909dd0e2bcc639f7f194170aeb8666898df
andre27593 <dam.aolivero@institutoserpis.org>
2016-09-20 10:03:59 +0200

    Añadido PDbPrueba con menu de opciones

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -1,5 +1,10 @@
 using System;
 using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.Common;
+
+
 
 namespace PDbPrueba
 {
@@ -7,11 +12,163 @@ namespace PDbPrueba
 	{
 		public static void Main (string[] args)
 		{
+			//Conectar con la base de datos
 			Console.WriteLine ("Probando acceso a dbprueba");
-			MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
-			mySqlConnection.Open ();
-			//operaciones...
-			mySqlConnection.Close ();
+			IDbConnection dbConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
+
+			//Creación de un comando para poder insertar, eliminar, modificar...
+			IDbCommand dbcommand = dbConnection.CreateCommand ();
+
+			//Añadir a la base de datos
+			IDbDataParameter dbDataParameter = dbcommand.CreateParameter ();
+			IDbDataParameter dbDataParameter2 = dbcommand.CreateParameter ();
+
+			IDataReader query;
+
+			dbConnection.Open ();
+
+		
+			//Menú con las opciones de salir, insertar, editar, eliminar, listar todos
+
+			Console.Write ("0.SALIR" + "\n" + "1.NUEVO" + "\n" + "2.EDITAR" + "\n" + "3.ELIMINAR" + "\n" + "4.LISTAR TODOS" + "\n");
+
+
+			Boolean opc=true;
+
+			do {
+
+				Console.Write ("Seleccione una opción: ");
+
+				switch (Console.Read ()) {
+				case '0':
+
+					Console.WriteLine ("");
+					Console.Write ("SALIR");
+					Console.WriteLine ("");
+					Environment.Exit (0);
+					break;
+
+				case '1':
+
+					Console.WriteLine ("");
+					Console.Write ("NUEVO");
+					Console.WriteLine ("");
+					Console.WriteLine ("Introduce el nombre: ");
+					String nombre = Console.ReadLine ();		
+					dbcommand.CommandText = "insert into categoria (id, nombre) values (id, @nombre)";
+
+
+					dbDataParameter.ParameterName = "nombre";
+
+					dbDataParameter.Value = nombre;
+					dbcommand.Parameters.Add (dbDataParameter);
+
+
+					dbcommand.ExecuteNonQuery ();
+
+					break;
+
+				case '2':
+
+					Console.WriteLine ("");
+					Console.Write ("EDITAR");
+					Console.WriteLine ("");
+					dbcommand.CommandText = "select  id, nombre from categoria";
+
+					query = dbcommand.ExecuteReader ();
+
+					while (query.Read()) {
+
+						int id = query.GetInt32 (0);
+						String nom = query.GetString (1);
+
+						Console.WriteLine ("El id es " + id + " y el nombre es " + nom);
+				
+
+					}
+
+
+					query.Close ();
+
+					Console.Write ("Dime el nombre nuevo");
+					String nombrenuevo = Console.ReadLine ();
+					Console.Write ("Dime el id del nombre que quieres modificar");
+					String idmod = Console.ReadLine ();
+					int idori = int.Parse (idmod); //Convertir el id de String a int
+
+
+					dbcommand.CommandText = "update categoria set nombre = @nombrenuevo where id = @id";
+
+					dbDataParameter.ParameterName = "nombrenuevo";
+
+					dbDataParameter.Value = nombrenuevo;
+
+
+					dbDataParameter2.ParameterName = "id";
+
+					dbDataParameter2.Value = idori;
+					dbcommand.Parameters.Add (dbDataParameter);
+					dbcommand.Parameters.Add (dbDataParameter2);
+
+
+
+					dbcommand.ExecuteNonQuery ();
+
+
+					break;
+
+
+				case '3':
+
+					Console.WriteLine ("");
+					Console.Write ("ELIMINAR");
+					Console.WriteLine ("");
+
+					Console.Write ("Dime el nombre que quieres eliminar");
+					String nombreeli = Console.ReadLine ();
+
+					dbcommand.CommandText = "delete from categoria where nombre=@nombreeli";
+
+					dbDataParameter.ParameterName = "nombreeli";
+					dbDataParameter.Value = nombreeli;
+
+					dbcommand.Parameters.Add (dbDataParameter);
+
+					dbcommand.ExecuteNonQuery ();
+
+					break;
+
+				case '4':
+
+					Console.WriteLine ("");
+					Console.Write ("LISTAR TODOS");
+					Console.WriteLine ("");
+
+					dbcommand.CommandText = "select  id, nombre from categoria";
+
+					query = dbcommand.ExecuteReader ();
+
+					while (query.Read()) {
+
+						int id = query.GetInt32 (0);
+						String nom = query.GetString (1);
+
+						Console.WriteLine ("El id es " + id + " y el nombre es " + nom);
+
+
+					}
+
+
+					query.Close ();
+
+					break;
+
+				}
+
+
+			} while(opc=true);
+
+			dbConnection.Close ();
 		}
 	}
 }
