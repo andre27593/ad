122b0c82555065c42c196c639baabdbfc5bad8b8
andre27593 <dam.aolivero@institutoserpis.org>
2016-10-11 12:20:18 +0200

    PTreeView hecho por Luis mejorado

--- Org.InstitutoSerpis.Ad/Org.InstitutoSerpis.Ad/TreeViewHelper.cs
+++ Org.InstitutoSerpis.Ad/Org.InstitutoSerpis.Ad/TreeViewHelper.cs
@@ -1,5 +1,9 @@
 using Gtk;
+using System.Collections;
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Org.InstitutoSerpis.Ad
 {
@@ -23,6 +27,60 @@ namespace Org.InstitutoSerpis.Ad
 			}
 
 		}
-	}
-}
+
+
+		public static void AppendColumns(TreeView treeView, Type type){
+			PropertyInfo[] propertyInfos = type.GetProperties ();
+			List<string> propertyNames = new List<string> ();
+			foreach (PropertyInfo propertyInfo in propertyInfos)
+				propertyNames.Add (propertyInfo.Name);
+			AppendColumns (treeView, propertyNames.ToArray ());
+
+
+		}
+
+		private static void appendColumns(TreeView treeView, IList list){
+			if (treeView.Columns != 0)
+				return;
+			Type listType = list.GetType ();
+			Type elementType = listType.GetGenericArguments () [0];
+			PropertyInfo[] propertyInfos = elementType.GetProperties ();
+			foreach (PropertyInfo propertyInfo in propertyInfos) {
+				string columnName = propertyInfo.Name;
+				treeView.AppendColumn (columnName, new CellRendererText (),
+				                       delegate(TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+					object item = tree_model.GetValue (iter, 0);
+					object value = propertyInfo.GetValue (item, null);
+					CellRendererText cellRendererText = (CellRendererText)cell;
+					cellRendererText.Text = value == null ? "" : value.ToString ();
+				}
+				);
+			}	
+		
+		}
+	
+
+		private static void appendValues (TreeView treeView, IList list) {
+
+			ListStore listStore = new ListStore (typeof(object));
+			treeView.Model = listStore;
+			foreach(object item in list){
+				listStore.AppendValues (item);
+
+			}
+		}
+
+		public static void Fill (TreeView treeView, IList list){
+
+			appendColumns (treeView, list);
+			appendValues (treeView, list);
+
+		}
+		
+		
+	
+
+
+
+
 
--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -1,19 +1,38 @@
 using Org.InstitutoSerpis.Ad;
 using System;
 using Gtk;
+using System.Collections.Generic;
+using System.Collections;
+
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		TreeViewHelper.AppendColumns (treeView, new string[] { "id", "nombre" });
-		ListStore listStore = new ListStore (typeof(long), typeof(string));
 
-		listStore.AppendValues (1L, "categoria 1");
-		listStore.AppendValues (2L, "categoria 2");
+		//IList list = new List<Articulo> ();
 
-		treeView.Model = listStore;
+
+		//list.Add (new Articulo(1L, "articulo 1",1.5m));
+		//list.Add (new Articulo(1L, "articulo 1",1.5m));
+		//list.Add (new Articulo(1L, "articulo 1",1.5m));
+
+		IList list = new List<Categoria> ();
+
+		list.Add (new Categoria(1L, "categoria 1"));
+		list.Add (new Categoria(2L, "categoria 2"));
+		list.Add (new Categoria(3L, "categoria 3"));
+
+		TreeViewHelper.Fill (treeView, list);
+
+		//TreeViewHelper.AppendColumns (treeView, typeof(Articulo));
+		//ListStore listStore = new ListStore (typeof(long), typeof(string));
+
+		//listStore.AppendValues (1L, "categoria 1");
+		//listStore.AppendValues (2L, "categoria 2");
+
+		//treeView.Model = listStore;
 
 	}
 
@@ -23,3 +42,28 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 }
+
+	public class Categoria{
+		public Categoria(long id, string nombre){
+			Id = id;
+			Nombre = nombre;
+		}
+
+		public long Id { get; set; }
+		public String Nombre { get; set; }
+	}
+
+	public class Articulo{
+		public Articulo(long id, string nombre, decimal precio){
+			Id = id;
+			Nombre = nombre;
+			Precio = precio;
+		}
+
+		public long Id { get; set; }
+		public String Nombre { get; set; }
+		public decimal Precio { get; set;}
+
+	}
+
+
