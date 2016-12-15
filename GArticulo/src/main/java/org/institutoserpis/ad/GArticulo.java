256bd1d1ea844801220dc58c5bdf5717e460528d
andre27593 <dam.aolivero@institutoserpis.org>
2016-12-15 09:22:36 +0100

    GArticulo hecho por mi acabado

--- GArticulo/src/main/java/org/institutoserpis/ad/GArticulo.java
+++ GArticulo/src/main/java/org/institutoserpis/ad/GArticulo.java
@@ -3,9 +3,12 @@ package org.institutoserpis.ad;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
+import com.mysql.jdbc.RowDataCursor;
 
 public class GArticulo {
 	
@@ -19,7 +22,8 @@ public class GArticulo {
 		scanner = new Scanner(System.in);
 		int opc = 0;
 		
-		System.out.println("Menú");
+		System.out.println("MENÚ");
+		System.out.println("----------------------------");
 		System.out.println("1. Nuevo");
 		System.out.println("2. Modificar");
 		System.out.println("3. Eliminar");
@@ -27,40 +31,47 @@ public class GArticulo {
 		System.out.println("5. Listar");
 		System.out.println("0. Salir");
 		
-		System.out.println("Escoge una opción ");
-		opc = scanner.nextInt();
+		System.out.println("-----------------------------");
 		
-		switch (opc) {
-		case 1:
-			System.out.println("Introducir nuevo articulo");
-			nuevo();
-			break;
+		do{
+			System.out.println("Escoge una opción: ");
+			opc = scanner.nextInt();
 		
-		case 2:
+			switch (opc) {
+			case 1:
+				System.out.println("Vas a introducir un nuevo artículo");
+				nuevo();
+				break;
+		
+			case 2:
+				System.out.println("Vas a modificar un articulo");
+				modificar();
+				break;
 			
-			break;
+			case 3:
+				System.out.println("Vas a eliminar un artículo");
+				eliminar();
+				break;
 			
-		case 3:
+			case 4:
+				System.out.println("Vas a consultar un artículo de tu base de datos");
+				consultar();
+				break;
 			
-			break;
+			case 5:
+				System.out.println("Vas a listar los artículos de tu base de datos");
+				listar();
+				break;
 			
-		case 4:
-			
-			break;
-			
-		case 5:
-			
-			break;
-			
-		case 0:
-			
-			break;
+			case 0:
+				System.out.println("Has salido del programa");
+				break;
 
-		default:
-			System.out.println("Opción incorrecta");
-			break;
-		}
-		
+			default:
+				System.out.println("Opción incorrecta");
+				break;
+			}
+		}while(opc!=0);
 		
 	}
 	
@@ -86,13 +97,111 @@ public class GArticulo {
 		preparedStatement.setDouble(2, precio);
 		preparedStatement.setInt(3, categoria);
 		
+		System.out.println();
+		
 		preparedStatement.executeUpdate();
 		preparedStatement.close();
 		
 		
 	}
 	
-	public static void modificar(){
+	public static void modificar() throws SQLException{
+		
+		String nomnuevo;
+		double prenuevo;
+		int catenuevo;		
+		int id;
+		
+		System.out.println("Dime el id del artículo que quieres modificar ");
+		id = scanner.nextInt();
+		
+		System.out.println("Dime el nombre  ");
+		nomnuevo=scanner.next();
+		
+		System.out.println("Dime el precio ");
+		prenuevo = scanner.nextDouble();
+		
+		System.out.println("Dime el id de la categoria ");
+		catenuevo = scanner.nextInt();
+		
+		String sql = "update articulo set nombre=?, precio=?, categoria=? where id='"+id+"'";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		
+		preparedStatement.setString(1, nomnuevo);
+		preparedStatement.setDouble(2, prenuevo);
+		preparedStatement.setInt(3, catenuevo);
+		
+		System.out.println();
+		
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+		
+	}
+	
+
+	public static void eliminar() throws SQLException{	
+		int id;
+		
+		System.out.println("Dime el id del artículo que quieres eliminar ");
+		id = scanner.nextInt();
+				
+		String sql = "delete from articulo where id='"+id+"'";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		
+		System.out.println();
+		
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+		
+	}
+	
+	public static void consultar() throws SQLException{
+		
+		int id;
+		
+		System.out.println("Dime el id del artículo que quieres consultar ");
+		id = scanner.nextInt();
+				
+		String sql = "select * from articulo where id='"+id+"'";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while (resultSet.next()){
+			
+			System.out.println("Id: " +resultSet.getInt("id"));
+			System.out.println("Nombre: " +resultSet.getString("nombre"));
+			System.out.println("Precio: " +resultSet.getDouble("precio"));
+			System.out.println("Categoria: " +resultSet.getInt("categoria"));
+			System.out.println();
+			
+		}
+				
+		resultSet.close();
+		preparedStatement.close();
+		
+	}
+	
+	public static void listar() throws SQLException{
+				
+		String sql = "select * from articulo";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while (resultSet.next()){
+			
+			System.out.println("Id: " +resultSet.getInt("id"));
+			System.out.println("Nombre: " +resultSet.getString("nombre"));
+			System.out.println("Precio: " +resultSet.getDouble("precio"));
+			System.out.println("Categoria: " +resultSet.getInt("categoria"));
+			System.out.println();
+			
+		}
+				
+		resultSet.close();
+		preparedStatement.close();
+		
 		
 	}
 
