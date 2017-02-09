62ea0f33e71d61598a00a7a9159ac9716ef5764b
andre27593 <dam.aolivero@institutoserpis.org>
2017-02-09 11:22:31 +0100

    HPedido

--- HPedido/src/main/java/org/institutoserpis/ad/Hibernate.java
+++ HPedido/src/main/java/org/institutoserpis/ad/Hibernate.java
@@ -41,127 +41,83 @@ public class Hibernate {
 		
 			switch (opc) {
 			case 1:
+				int opci=0;
 				System.out.println("Que quieres insertar? ");
 				System.out.println();
 				System.out.println("7. Cliente");
 				System.out.println("8. Pedido");
 				System.out.println("9. Salir");
+				opci = scanner.nextInt();
 				
-				int opci=0;
+				if(opci == 7){
+						System.out.println("Vas a insertar un cliente");
+						String nombre;
+						System.out.println("Dime el nombre ");
+						nombre=scanner.next();
+						nuevo_cliente(nombre);
+				}
 				
-				do {
+				if(opci == 8){
 					
-					switch (opci) {
-					case 9:
+						System.out.println("Vas a insertar un pedido");
+						BigDecimal importe;
+						System.out.println("Dime el importe del pedido ");
+						importe = scanner.nextBigDecimal();
+						//nuevo_pedido(id,importe);
+						
+				}
+				
+				if(opci == 9){
+					
 						System.out.println("Has salido de la opción insertar");
 						break;
-					case 7:
-						System.out.println("Vas a insertar un cliente");
-						nuevo_cliente();
-					case 8:
-						System.out.println("Vas a insertar un pedido");
-						nuevo_pedido();
-
-					default:
-						break;
-					}
-					
-					
-				} while (opci!=9);
-		
-			case 2:
-				System.out.println("Vas a modificar un articulo");
-				System.out.println();
-				//listar();
-				//modificar();
-				break;
-			
-			case 3:
-				System.out.println("Vas a eliminar un artículo");
-				System.out.println();
-				//eliminar();
-				break;
-			
-			case 4:
-				System.out.println("Vas a consultar un artículo de tu base de datos");
-				System.out.println();
-				//consultar();
-				break;
-			
-			case 5:
-				System.out.println("Vas a listar los artículos de tu base de datos");
-				System.out.println();
-				//listar();
-				break;
-			
+				}
+				
 			case 0:
-				System.out.println("Has salido del programa");
-				break;
-
+				System.out.println("Has salido del menú");
+				break;			
 			default:
-				System.out.println("Opción incorrecta");
 				break;
 			}
 		}while(opc!=0);
 		
 }
 
-	public static void nuevo_cliente(){
+	public static void nuevo_cliente(String nombre){
 			
 		entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		
-		String nombre;
 			
-		System.out.println("Dime el nombre ");
-		nombre=scanner.next();
-			
-		
 		Cliente cliente = new Cliente();
 		cliente.setNombre(nombre);
 		entityManager.persist(cliente);
 		
+		entityManager.getTransaction().commit();
 		entityManager.close();
 			
 	}
 	
-	public static void nuevo_pedido(){
+	public static void nuevo_pedido(String idcliente, BigDecimal importe){
 		
 		entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+		Cliente cliente = entityManager.getReference(Cliente.class, Long.parseLong(idcliente));
 		
-		String nombre;
-			
-		System.out.println("Dime el nombre ");
-		nombre=scanner.next();
-			
-		
-		Cliente cliente = new Cliente();
-		cliente.setNombre(nombre);
-		entityManager.persist(cliente);
-		
-		entityManager.close();
-			
-	}
-	
-		
-
-	/*	//Insertar pedido
 		Pedido pedido = new Pedido();
 		pedido.setCliente(cliente);
+		
 		java.util.Date dat = Calendar.getInstance().getTime();
 		Date date = new Date(dat.getDate());
 		pedido.setFecha(date);
-		BigDecimal importe = new BigDecimal(50.40);
 		pedido.setImporte(importe);
 		entityManager.persist(pedido);
-				
-
-		entityManager.getTransaction().commit();
+			
+		entityManager.getTransaction().commit();		
 		entityManager.close();
-		
-		entityManagerFactory.close();
-		
+			
 	}
-*/
+	
+		
+
+
 }
