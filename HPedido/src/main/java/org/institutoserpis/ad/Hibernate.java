8d5e0f052e7b60562136093aec3cc816849791aa
andre27593 <dam.aolivero@institutoserpis.org>
2017-02-14 10:13:00 +0100

    HPedido con consultar, listar, insertar, modificar y eliminar

--- HPedido/src/main/java/org/institutoserpis/ad/Hibernate.java
+++ HPedido/src/main/java/org/institutoserpis/ad/Hibernate.java
@@ -1,15 +1,22 @@
 package org.institutoserpis.ad;
 
+import java.awt.event.ItemEvent;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Persistence;
+
+import org.hibernate.engine.internal.StatefulPersistenceContext;
+import org.hibernate.query.criteria.internal.expression.function.SubstringFunction;
+
+
 
 public class Hibernate {
 	
@@ -57,17 +64,23 @@ public class Hibernate {
 						nuevo_cliente(nombre);
 				}
 				
-				if(opci == 8){
+				else if(opci == 8){
 					
+						System.out.println("Lista de clientes");
+						listar_clientes();
+						System.out.println();
 						System.out.println("Vas a insertar un pedido");
 						BigDecimal importe;
+						String idcliente;
+						System.out.println("Dime el id del cliente ");
+						idcliente = scanner.next();
 						System.out.println("Dime el importe del pedido ");
 						importe = scanner.nextBigDecimal();
-						//nuevo_pedido(id,importe);
+						nuevo_pedido(idcliente,importe);
 						
 				}
 				
-				if(opci == 9){
+				else if(opci == 9){
 					
 						System.out.println("Has salido de la opción insertar");
 						break;
@@ -75,7 +88,139 @@ public class Hibernate {
 				
 			case 0:
 				System.out.println("Has salido del menú");
-				break;			
+				break;	
+			
+			case 2:
+				int opcion=0;
+				System.out.println("Que quieres eliminar? ");
+				System.out.println();
+				System.out.println("7. Cliente");
+				System.out.println("8. Pedido");
+				System.out.println("9. Salir");
+				opcion = scanner.nextInt();
+				
+				if(opcion == 7){
+						
+						
+				}
+				
+				else if(opcion == 8){
+					
+						
+				}
+				
+				else if(opcion == 9){
+					
+						System.out.println("Has salido de la opción eliminar");
+						break;
+				}
+				
+			case 3:
+				int opcio=0;
+				System.out.println("Que quieres eliminar? ");
+				System.out.println();
+				System.out.println("7. Cliente");
+				System.out.println("8. Pedido");
+				System.out.println("9. Salir");
+				opcio = scanner.nextInt();
+				
+				if(opcio == 7){
+						System.out.println("Vas a eliminar un cliente");
+						System.out.println();
+						System.out.println("Lista de clientes");
+						listar_clientes();
+						System.out.println();
+						String idcliente;
+						System.out.println("Dime el id del cliente que quieres eliminar ");
+						idcliente=scanner.next();
+						eliminar_cliente(idcliente);
+						
+				}
+				
+				else if(opcio == 8){
+					
+						System.out.println("Vas a eliminar un pedido");
+						System.out.println();
+						System.out.println("Lista de pedidos");
+						listar_pedidos();
+						String idpedido;
+						System.out.println("Dime el id del cliente ");
+						idpedido = scanner.next();
+						eliminar_pedido(idpedido);
+						
+				}
+				
+				else if(opcio == 9){
+					
+						System.out.println("Has salido de la opción eliminar");
+						break;
+				}
+				
+			case 4:
+				int opn=0;
+				System.out.println("Que quieres consultar? ");
+				System.out.println();
+				System.out.println("7. Cliente");
+				System.out.println("8. Pedido");
+				System.out.println("9. Salir");
+				opn = scanner.nextInt();
+				
+				if(opn == 7){
+					System.out.println("Vas a consultar un cliente");
+					System.out.println();
+					String idcliente;
+					System.out.println("Dime el id del cliente ");
+					idcliente = scanner.next();
+					consultar_cliente(idcliente);
+						
+				}
+				
+				else if(opn == 8){
+					System.out.println("Vas a consultar un pedido");
+					System.out.println();
+					String idpedido;
+					System.out.println("Dime el id del pedido ");
+					idpedido = scanner.next();
+					consultar_pedido(idpedido);						
+						
+						
+				}
+				
+				else if(opn == 9){
+					
+						System.out.println("Has salido de la opción consultar");
+						break;
+				}
+				
+			case 5:
+				int o=0;
+				System.out.println("Que quieres listar? ");
+				System.out.println();
+				System.out.println("7. Cliente");
+				System.out.println("8. Pedido");
+				System.out.println("9. Salir");
+				o = scanner.nextInt();
+				
+				if(o == 7){
+						System.out.println("Clientes");
+						listar_clientes();
+						
+				}
+				
+				else if(o == 8){
+					
+						System.out.println("Pedidos");
+						listar_pedidos();
+						
+				}
+				
+				else if(o == 9){
+					
+						System.out.println("Has salido de la opción listar");
+						break;
+				}
+				
+				
 			default:
 				break;
 			}
@@ -111,12 +256,127 @@ public class Hibernate {
 		pedido.setFecha(date);
 		pedido.setImporte(importe);
 		entityManager.persist(pedido);
-			
+		
 		entityManager.getTransaction().commit();		
 		entityManager.close();
 			
 	}
 	
+	public static void listar_clientes(){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Cliente cliente = new Cliente();
+		
+		System.out.printf("%d %s\n", cliente.getId(), cliente.getNombre());
+		
+		List<Cliente> clientes = entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+		
+		for(Cliente item : clientes){
+			System.out.printf("%d %s\n", item.getId(), item.getNombre());
+		}
+		
+			
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+		
+	}
+	
+	public static void listar_pedidos(){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Pedido pedido = new Pedido();
+		
+		System.out.printf("%d %s %s %s\n", pedido.getId(), pedido.getCliente(), pedido.getFecha(), pedido.getImporte());
+		
+		List<Pedido> pedidos = entityManager.createQuery("from Pedido", Pedido.class).getResultList();
+		for(Pedido item : pedidos){
+			System.out.printf("%d %s %s %s %s\n", item.getId(), item.getCliente() , item.getFecha(), item.getImporte());
+		}
+		
+			
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+	}
+	
+	public static void eliminar_cliente(String idcliente){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Cliente cliente = entityManager.getReference(Cliente.class, Long.parseLong(idcliente));
+		
+		entityManager.remove(cliente);
+		
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+	}
+	
+	public static void eliminar_pedido(String idpedido){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Pedido pedido = entityManager.getReference(Pedido.class, Long.parseLong(idpedido));
+		
+		entityManager.remove(pedido);
+		
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+	}
+	
+	public static void consultar_pedido(String idpedido){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+				
+		Pedido pedido = entityManager.getReference(Pedido.class, Long.parseLong(idpedido));
+		System.out.printf("%d %s %s %s\n", pedido.getId(), pedido.getCliente(), pedido.getFecha(), pedido.getImporte());
+			
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+	}
+	
+	public static void consultar_cliente(String idcliente){
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+				
+		Cliente cliente = entityManager.getReference(Cliente.class, Long.parseLong(idcliente));
+		System.out.printf("%d %s\n", cliente.getId(), cliente.getNombre());
+			
+		entityManager.getTransaction().commit();		
+		entityManager.close();
+		
+		
+	}
+	
+	public static void modificar_cliente(String idcliente){
+		
+		
+		
+	}
+	
+	public static void modificar_pedido(String idpedido){
+		
+		
+		
+	}
 		
 
 
